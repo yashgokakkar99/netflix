86ed4d03345cd06f47fc5a8550b5d2a5aeac5e76
Yash Gokakkar <yashgokakkar@gmail.com>
2023-08-11 23:22:24 +0530

    Netflix UI designed

---
+++ netflix/src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
